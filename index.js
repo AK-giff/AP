49dbed17e2748f6e59831f7ab904f8b09b00647e
AK-giff <7alphasquad@gmail.com>
2026-04-19 19:07:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const Lead = require("./Lead");
 const app = express();
 app.use(express.json());
 
+const port = process.env.PORT || 4000 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+
 // DB connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB connected"))
