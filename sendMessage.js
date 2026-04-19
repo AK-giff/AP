8aa888d0c574ecae7ed1ce98c14bfe5cae835e2d
AK-giff <7alphasquad@gmail.com>
2026-04-19 14:13:19 +0530

    Create sendMessage.js

---
+++ sendMessage.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+async function sendMessage(phone, message) {
+  await axios.post(
+    https://graph.facebook.com/v18.0/YOUR_PHONE_ID/messages,
+    {
+      messaging_product: "whatsapp",
+      to: phone,
+      text: { body: message }
+    },
+    {
+      headers: {
+        Authorization: Bearer ${process.env.WHATSAPP_TOKEN}
+      }
+    }
+  );
+}
